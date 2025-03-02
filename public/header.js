523a24a7822cc2e40219c105e352510993b8637e
Fiandralazuart <138683498+Fiandralazuart@users.noreply.github.com>
2025-03-02 16:53:29 +0700

    Delete public directory

--- public/header.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById('menu-icon')
-const nav = document.querySelector('.menu-container')
-
-menu.addEventListener('click', () => {
-   nav.classList.toggle('hidden')
-})
